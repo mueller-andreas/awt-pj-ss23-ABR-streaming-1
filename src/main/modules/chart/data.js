cb4486e1966da4349c55ac9ba7b4a352c4c46751
Jakob Zerbian <j.zerbian@tu-berlin.de>
2023-06-26 17:46:56 +0200

    set color callback functions as object properties

--- src/main/modules/chart/data.js
+++ src/main/modules/chart/data.js
@@ -8,20 +8,9 @@ export const data = {
         { x: 10000, y: 1000 },
         { x: 20000, y: 12000 },
       ],
-
-      borderColor: (ctx) =>
-        // console.log(ctx);
-        borderStyle(ctx, 'rgb(192,75,75)'),
-      // borderColor: "rgb(67, 155, 255)",
-      // backgroundColor: "rgba(67, 155, 255, 0.5)",
-      backgroundColor: (ctx) =>
-        // console.log(ctx);
-        pointStyle(ctx, 'rgba(192,75,75, 0.5)'),
       stepped: 'after',
-      segment: {
-        borderColor: (ctx) => segmentStyle(ctx, 'rgb(192,75,75)'),
-        // backgroundColor: (ctx) => segmentStyle(ctx, "rgb(192,75,75, 0.5)"),
-      },
+      segment: {},
+      currentSeg: -1,
 
       // Disable the first point
       pointHitRadius(context) {
@@ -34,10 +23,6 @@ export const data = {
   ],
 };
 
-const currentSeg = 2;
-
-const segmentStyle = (ctx, value) => (ctx.p1DataIndex === currentSeg ? value : undefined);
-
-const borderStyle = (ctx, value) => (ctx.dataIndex === currentSeg ? value : 'rgb(67, 155, 255)');
-
-const pointStyle = (ctx, value) => (ctx.dataIndex === currentSeg ? value : 'rgba(67, 155, 255, 0.5)');
+data.datasets[0].borderColor = (ctx) => (ctx.dataIndex === data.datasets[0].currentSeg ? 'rgb(192,75,75)' : 'rgb(67, 155, 255)');
+data.datasets[0].backgroundColor = (ctx) => (ctx.dataIndex === data.datasets[0].currentSeg ? 'rgba(192,75,75, 0.5)' : 'rgba(67, 155, 255, 0.5)');
+data.datasets[0].segment.borderColor = (ctx) => (ctx.p1DataIndex === data.datasets[0].currentSeg ? 'rgb(255,0,0)' : undefined);
