0434478fc98e82f7794eef208b247f4342593bb8
Jakob Zerbian <j.zerbian@tu-berlin.de>
2023-07-31 17:57:04 +0200

    remove more comments

--- src/editor.js
+++ src/editor.js
@@ -51,22 +51,17 @@ updateDataAndUI(chart);
 // Get the context menu element
 const contextMenu = document.getElementById('contextMenu');
 
-// Add an event listener for the contextmenu event
 chart.canvas.addEventListener('contextmenu', (event) => handleContextMenu(event, chart, contextMenu));
 
-// Add an event listener for the dataChanged event
-// using the imported function and passing the chart object as a parameter
 chart.canvas.addEventListener('dataChanged', () => {
   updateDataAndUI(chart);
   saveChartData(chart);
 });
 
-// Add event listener for click event on document to hide context menu
 document.addEventListener('click', () => {
   contextMenu.style.display = 'none';
 });
 
-// event listener for changes in the textarea contents
 const chartText = document.getElementById('chartData');
 
 chartText.addEventListener('input', (event) => {
@@ -91,28 +86,22 @@ chartText.addEventListener('focusout', (event) => {
 
 formatJSONText(chartText);
 
-// Copy current text to clipboard
 document.querySelector('#copy-button').onclick = () => {
   navigator.clipboard.writeText(document.querySelector('#chartData').innerText);
 };
 
-// Add an event listener to the export button
 document
   .getElementById('graphicButton')
   .addEventListener('click', () => exportGraphic(chart));
 
-// Add an event listener to the json export button
 document
   .getElementById('jsonButtonExport')
   .addEventListener('click', () => exportChartData(chart));
 
-// Add an event listener to the zoomGraphButton button
 document
   .getElementById('zoomGraphButton')
   .addEventListener('click', () => zoomToGraph(chart));
 
-// Add an event listener to the json import button
-// Use updateDataAndUI as callback function for updating the chart and textfield after import
 
 document.getElementById('jsonButtonImport').addEventListener('click', () => {
   importChartData(chart, updateDataAndUI, saveChartData);
